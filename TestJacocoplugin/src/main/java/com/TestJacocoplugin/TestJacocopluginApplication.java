a454ea73ec5b28bbd610edd8cf7af7f4218eab71
Kiran Mayee <sree.kironmai@gmail.com>
2024-04-26 11:51:57 -0500

    SB maven proj for jacoco added

---
+++ TestJacocoplugin/src/main/java/com/TestJacocoplugin/TestJacocopluginApplication.java
@@ -0,0 +1,13 @@
+package com.TestJacocoplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJacocopluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJacocopluginApplication.class, args);
+	}
+
+}
