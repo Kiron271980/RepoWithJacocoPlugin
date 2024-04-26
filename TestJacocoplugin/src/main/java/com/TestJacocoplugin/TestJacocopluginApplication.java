7ab3d7bc53976850a3b16ffaa33c7ccbcc4b2562
Kiran Mayee <sree.kironmai@gmail.com>
2024-04-26 13:39:35 -0500

    refactored code

--- TestJacocoplugin/src/main/java/com/TestJacocoplugin/TestJacocopluginApplication.java
+++ TestJacocoplugin/src/main/java/com/TestJacocoplugin/TestJacocopluginApplication.java
@@ -2,12 +2,13 @@ package com.TestJacocoplugin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.*.*")
 public class TestJacocopluginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestJacocopluginApplication.class, args);
 	}
-
 }
