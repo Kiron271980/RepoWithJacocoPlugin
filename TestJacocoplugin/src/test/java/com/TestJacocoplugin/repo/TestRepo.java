f556189c286adf54b1aa07c71cd118ad33f4d4dd
Kiran Mayee <sree.kironmai@gmail.com>
2024-04-29 17:57:51 -0500

    modified files

--- TestJacocoplugin/src/test/java/com/TestJacocoplugin/repo/TestRepo.java
+++ TestJacocoplugin/src/test/java/com/TestJacocoplugin/repo/TestRepo.java
@@ -1,5 +1,22 @@
 package com.TestJacocoplugin.repo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+//@AllArgsConstructor
+@ToString
 public class TestRepo {
 
-}
+	
+		public int id;
+		public String name;
+
+		public TestRepo(int i, String string) {
+			// TODO Auto-generated constructor stub
+		}
+	}
